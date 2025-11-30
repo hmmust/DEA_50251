1bcceb42078bf5aeaaf3a1b4ced6e563f4d912cd
Hossam <hossam.mustafa@uop.edu.jo>
2025-11-30 12:00:55 +0300

    v3

--- lect7.py
+++ lect7.py
@@ -4,16 +4,20 @@ import glob
 
 conn_string = "postgresql://admin:admin@localhost:5433/fitdb"
 
-files= glob.glob("./sample_data/retail_db/customers/*")
-print(files)
-
 s= open('./sample_data/retail_db/schemas.json')
 schemas = json.load(s)
-print(schemas['customers'])
-customers_schema= schemas['customers']
-customers_schema= sorted(customers_schema,key=lambda c:c['column_position'])
-cols = [ i['column_name'] for i in customers_schema]
-customers= pd.read_csv('./sample_data/retail_db/customers/part-00000',
-                       names=cols)
-customers.to_sql('customers',conn_string,index=False,if_exists='replace')
-print(customers)
+def get_column_names(schemas,ds):
+    ds_schema= schemas[ds]
+    ds_schema= sorted(ds_schema,key=lambda c:c['column_position'])
+    cols = [ i['column_name'] for i in ds_schema]
+    return cols
+for s in schemas:
+    cols = get_column_names(schemas,s)
+    files= glob.glob(f"./sample_data/retail_db/{s}/*")
+    print("loading schema:",s)
+    for f in files:
+        print("\tloading file:", f)
+        df= pd.read_csv(f,names=cols)
+        df.to_sql(s,conn_string,index=False,if_exists='append')
+print("loading completed!")
+
