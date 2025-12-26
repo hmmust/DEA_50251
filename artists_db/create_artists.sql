c5e84cfac761ffe98c932c9af1f159a865134f6c
hmmust <146467005+hmmust@users.noreply.github.com>
2025-12-27 01:07:30 +0300

    Add files via upload

---
+++ artists_db/create_artists.sql
@@ -0,0 +1,110 @@
+CREATE TABLE  IF NOT EXISTS artists (
+    "ConstituentID" bigint,
+    "DisplayName" text,
+    "ArtistBio" text,
+    "Nationality" text,
+    "Gender" text,
+    "BeginDate" bigint,
+    "EndDate" bigint,
+    "Wiki QID" text,
+    "ULAN" double precision
+);
+
+INSERT INTO public.artists VALUES (1, 'Robert Arneson', 'American, 1930–1992', 'American', 'male', 1930, 1992, NULL, NULL);
+INSERT INTO public.artists VALUES (2, 'Doroteo Arnaiz', 'Spanish, born 1936', 'Spanish', 'male', 1936, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (3, 'Bill Arnold', 'American, born 1941', 'American', 'male', 1941, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (4, 'Charles Arnoldi', 'American, born 1946', 'American', 'male', 1946, 0, 'Q1063584', 500027998);
+INSERT INTO public.artists VALUES (5, 'Per Arnoldi', 'Danish, born 1941', 'Danish', 'male', 1941, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (6, 'Danilo Aroldi', 'Italian, born 1925', 'Italian', 'male', 1925, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (7, 'Bill Aron', 'American, born 1941', 'American', 'male', 1941, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (9, 'David Aronson', 'American, born Lithuania 1923', 'American', 'male', 1923, 0, 'Q5230870', 500003363);
+INSERT INTO public.artists VALUES (10, 'Irene Aronson', 'American, born Germany 1918', 'American', 'female', 1918, 0, 'Q19748568', 500042413);
+INSERT INTO public.artists VALUES (11, 'Jean (Hans) Arp', 'French, born Germany (Alsace). 1886–1966', 'French', 'male', 1886, 1966, 'Q153739', 500031000);
+INSERT INTO public.artists VALUES (12, 'Jüri Arrak', 'Estonian, born 1936', 'Estonian', 'male', 1936, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (13, 'José Arellano Fischer', 'Mexican, 1911–1995', 'Mexican', 'male', 1911, 1995, NULL, NULL);
+INSERT INTO public.artists VALUES (15, 'Folke Arstrom', 'Swedish, 1907–1997', 'Swedish', 'male', 1907, 1997, NULL, NULL);
+INSERT INTO public.artists VALUES (16, 'Cristobal Arteche', 'Spanish, 1900–1964', 'Spanish', 'male', 1900, 1964, NULL, NULL);
+INSERT INTO public.artists VALUES (18, 'Artko', NULL, NULL, NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (19, 'Richard Artschwager', 'American, 1923–2013', 'American', 'male', 1923, 2013, 'Q568262', 500114981);
+INSERT INTO public.artists VALUES (21, 'Ruth Asawa', 'American, 1926–2013', 'American', 'female', 1926, 2013, 'Q7382874', 500077806);
+INSERT INTO public.artists VALUES (22, 'Isidora Aschheim', 'Israeli', 'Israeli', 'female', 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (23, 'Charles Robert Ashbee', 'British, 1863–1942', 'British', 'male', 1863, 1942, 'Q614071', 500030367);
+INSERT INTO public.artists VALUES (24, 'Donald Ashcraft', 'American, born 1927', 'American', 'male', 1927, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (25, 'E. M. Ashe', 'American, 1867–1941', 'American', 'male', 1867, 1941, NULL, NULL);
+INSERT INTO public.artists VALUES (26, 'Göran Åslin', 'Swedish, born 1940', 'Swedish', NULL, 1940, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (27, 'Erik Gunnar Asplund', 'Swedish, 1885–1940', 'Swedish', 'male', 1885, 1940, 'Q312070', 500011232);
+INSERT INTO public.artists VALUES (28, 'Geneviève Asse', 'French, born 1923', 'French', 'female', 1923, 0, 'Q1978192', 500025324);
+INSERT INTO public.artists VALUES (30, 'Sergio Asti', 'Italian, 1926–2021', 'Italian', 'male', 1926, 2021, NULL, NULL);
+INSERT INTO public.artists VALUES (31, 'Dana Atchley', 'American, 1941–2000', 'American', 'female', 1941, 2000, NULL, NULL);
+INSERT INTO public.artists VALUES (32, 'Atelier Eggers', 'French', 'French', NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (33, 'A.A.P.', 'American', 'American', NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (34, 'Alvar Aalto', 'Finnish, 1898–1976', 'Finnish', 'male', 1898, 1976, 'Q82840', 500002617);
+INSERT INTO public.artists VALUES (35, 'Aino Aalto', 'Finnish, 1894–1949', 'Finnish', 'female', 1894, 1949, 'Q273511', 500024110);
+INSERT INTO public.artists VALUES (36, 'Eero Aarnio', 'Finnish, born 1932', 'Finnish', 'male', 1932, 0, 'Q707025', 500270610);
+INSERT INTO public.artists VALUES (37, 'Jules Aarons', 'American, 1921–2008', 'American', 'male', 1921, 2008, NULL, NULL);
+INSERT INTO public.artists VALUES (38, 'Magdalena Abakanowicz', 'Polish, 1930–2017', 'Polish', 'female', 1930, 2017, 'Q158080', 500084577);
+INSERT INTO public.artists VALUES (39, 'James Abbe', 'American, 1883–1973', 'American', 'male', 1883, 1973, 'Q321301', 500026671);
+INSERT INTO public.artists VALUES (40, 'Jussuf Abbo', 'Palestinian, 1888–1953', 'Palestinian', 'male', 1888, 1953, NULL, NULL);
+INSERT INTO public.artists VALUES (41, 'Berenice Abbott', 'American, 1898–1991', 'American', 'female', 1898, 1991, 'Q231861', 500020631);
+INSERT INTO public.artists VALUES (42, 'Nobuya Abe', 'Japanese, 1913–1971', 'Japanese', 'male', 1913, 1971, NULL, NULL);
+INSERT INTO public.artists VALUES (43, 'Abe Shiro (pen Name: Suichiku)', 'Japanese, born 1900', 'Japanese', 'male', 1900, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (44, 'Robert Abel', 'American, born 1937', 'American', 'male', 1937, 0, 'Q7341326', NULL);
+INSERT INTO public.artists VALUES (46, 'Sigmund Abeles', 'American, born 1934', 'American', 'male', 1934, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (47, 'Raimund Abraham', 'American, born Austria. 1933–2010', 'American', 'male', 1933, 2010, 'Q78819', 500031964);
+INSERT INTO public.artists VALUES (49, 'Ivor Abrahams', 'British, born 1935', 'British', 'male', 1935, 0, 'Q11714362', 500012378);
+INSERT INTO public.artists VALUES (50, 'Gerald Abramovitz', 'British, born South Africa. 1928–2011', 'British', 'male', 1928, 2011, NULL, NULL);
+INSERT INTO public.artists VALUES (51, 'William Ross Abrams', 'American, 1920–2007', 'American', 'male', 1920, 2007, NULL, NULL);
+INSERT INTO public.artists VALUES (52, 'Rodolfo Abularach', 'Guatemalan, 1933–2020', 'Guatemalan', 'male', 1933, 2020, 'Q7357131', 500118427);
+INSERT INTO public.artists VALUES (53, 'Vito Acconci', 'American, 1940–2017', 'American', 'male', 1940, 2017, 'Q433471', 500014538);
+INSERT INTO public.artists VALUES (54, 'Milton Ackoff', 'American, born 1915', 'American', 'male', 1915, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (55, 'Norman Ackroyd', 'British', 'British', 'male', 0, 0, 'Q7051892', 500029028);
+INSERT INTO public.artists VALUES (56, 'Luis Alberto Acuña', 'Colombian, 1904–1994', 'Colombian', 'male', 1904, 1994, NULL, NULL);
+INSERT INTO public.artists VALUES (57, 'Henri Georges Adam', 'French, 1904–1967', 'French', 'male', 1904, 1967, 'Q662900', 500020748);
+INSERT INTO public.artists VALUES (58, 'Antoine Samuel Adam-Salomon', 'French, 1818–1881', 'French', 'male', 1818, 1881, NULL, NULL);
+INSERT INTO public.artists VALUES (59, 'Valerio Adami', 'Italian, born 1935', 'Italian', 'male', 1935, 0, 'Q562139', 500013534);
+INSERT INTO public.artists VALUES (60, 'Ansel Adams', 'American, 1902–1984', 'American', 'male', 1902, 1984, 'Q60809', 500026108);
+INSERT INTO public.artists VALUES (61, 'Alice Adams', 'American, born 1930', 'American', 'female', 1930, 0, 'Q4725709', 500033075);
+INSERT INTO public.artists VALUES (62, 'Clinton Adams', 'American, 1918–2002', 'American', 'male', 1918, 2002, 'Q5134058', 500042367);
+INSERT INTO public.artists VALUES (63, 'Dacres Adams', 'British, 1864–1951', 'British', 'male', 1864, 1951, NULL, NULL);
+INSERT INTO public.artists VALUES (64, 'Dennis Adams', 'American, born 1948', 'American', 'male', 1948, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (65, 'Kenneth M. Adams', 'American, 1897–1966', 'American', 'male', 1897, 1966, NULL, NULL);
+INSERT INTO public.artists VALUES (66, 'Robert Adams', 'American, born 1937', 'American', 'male', 1937, 0, 'Q1392710', 500037036);
+INSERT INTO public.artists VALUES (67, 'Shelby Lee Adams', 'American, born 1950', 'American', 'male', 1950, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (68, 'Charles Addams', 'American, 1912–1988', 'American', 'male', 1912, 1988, 'Q361106', 500074368);
+INSERT INTO public.artists VALUES (69, 'Allan Adler', 'American, 1916–2002', 'American', 'male', 1916, 2002, NULL, NULL);
+INSERT INTO public.artists VALUES (70, 'Jankel Adler', 'Polish, 1895–1949', 'Polish', 'male', 1895, 1949, 'Q213726', 500027359);
+INSERT INTO public.artists VALUES (71, 'Virginia Admiral', 'American, 1915–2000', 'American', 'female', 1915, 2000, 'Q7815833', 500044912);
+INSERT INTO public.artists VALUES (72, 'Pic Adrian', 'Romanian, born 1910', 'Romanian', 'male', 1910, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (74, 'Roy Adzak', 'British, 1927–1987', 'British', 'male', 1927, 1987, 'Q3445734', 500032997);
+INSERT INTO public.artists VALUES (76, 'Nicholas Africano', 'American, born 1948', 'American', 'male', 1948, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (77, 'Afro (Afro Basaldella)', 'Italian, 1912–1976', 'Italian', 'male', 1912, 1976, 'Q388090', 500014109);
+INSERT INTO public.artists VALUES (78, 'Aftograf', 'Russian', 'Russian', NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (79, 'Agam (Yaacov Agam)', 'Israeli, born 1928', 'Israeli', 'male', 1928, 0, 'Q276030', 500019224);
+INSERT INTO public.artists VALUES (80, 'Mehemed Fehmy Agha', 'American, born Russia. 1896–1978', 'American', 'male', 1896, 1978, 'Q3853982', 500032736);
+INSERT INTO public.artists VALUES (81, 'Kathleen Agnoli', 'American, born 1945', 'American', 'female', 1945, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (82, 'Peter Agostini', 'American, 1913–1993', 'American', 'male', 1913, 1993, NULL, NULL);
+INSERT INTO public.artists VALUES (83, 'Olympe Aguado de las Marismas', 'French, 1827–1894', 'French', 'male', 1827, 1894, 'Q2734214', 500345720);
+INSERT INTO public.artists VALUES (84, 'Dongkuk S. Ahn', 'American, 1937–2013', 'American', 'male', 1937, 2013, NULL, NULL);
+INSERT INTO public.artists VALUES (85, 'Aarne Aho', 'Finnish, 1904–1978', 'Finnish', 'male', 1904, 1978, NULL, NULL);
+INSERT INTO public.artists VALUES (86, 'Otl Aicher (also known as Otto Aicher)', 'German, 1922–1991', 'German', 'male', 1922, 1991, 'Q61191', 500058307);
+INSERT INTO public.artists VALUES (87, 'Lucien Aigner', 'American, born Hungary. 1901–1999', 'American', 'male', 1901, 1999, 'Q3265338', 500043259);
+INSERT INTO public.artists VALUES (88, 'Gregory Ain', 'American, 1908–1988', 'American', 'male', 1908, 1988, 'Q5606793', 500008108);
+INSERT INTO public.artists VALUES (89, 'Edgar Ainsworth', 'British, 1906–1975', 'British', 'male', 1906, 1975, NULL, NULL);
+INSERT INTO public.artists VALUES (91, 'Roberto Aizenberg', 'Argentine, 1928–1996', 'Argentine', 'male', 1928, 1996, NULL, NULL);
+INSERT INTO public.artists VALUES (92, 'Kinji Akagawa', 'American, born Japan 1940', 'American', 'male', 1940, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (93, 'Ryoji Akiyama', 'Japanese, born 1942', 'Japanese', 'male', 1942, 0, 'Q7385596', NULL);
+INSERT INTO public.artists VALUES (94, 'Munira Al-Kazi', 'Kuwaiti, born 1939', 'Kuwaiti', 'female', 1939, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (95, 'Alastair (Hans H. Voight)', 'German, 1887–1969', 'German', 'male', 1887, 1969, NULL, NULL);
+INSERT INTO public.artists VALUES (96, 'Anni Albers', 'American, born Germany. 1899–1994', 'American', 'female', 1899, 1994, 'Q86078', 500012591);
+INSERT INTO public.artists VALUES (97, 'Josef Albers', 'American, born Germany. 1888–1976', 'American', 'male', 1888, 1976, 'Q170071', 500033049);
+INSERT INTO public.artists VALUES (98, 'Beatrice Albert', 'American', 'American', 'female', 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (99, 'Joan Albert', 'American, 1943–2012', 'American', 'female', 1943, 2012, NULL, NULL);
+INSERT INTO public.artists VALUES (100, 'Ivan LeLorraine Albright', 'American, 1897–1983', 'American', 'male', 1897, 1983, 'Q1392676', 500016413);
+INSERT INTO public.artists VALUES (101, 'Album-miscellaneous', 'Various', NULL, NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (103, 'Albert Alcalay', 'American, 1917–2008', 'American', 'male', 1917, 2008, NULL, NULL);
+INSERT INTO public.artists VALUES (104, 'L. (Alfred Lewin Copley) Alcopley', 'American, born Germany. 1910–1992', 'American', 'male', 1910, 1992, 'Q3611295', 500019879);
+INSERT INTO public.artists VALUES (105, 'A. Alcorn', 'American', 'American', NULL, 0, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (106, 'Alan Aldridge', 'British, born 1943', 'British', 'male', 1943, 0, NULL, NULL);
+INSERT INTO public.artists VALUES (107, 'Pierre Alechinsky', 'Belgian, born 1927', 'Belgian', 'male', 1927, 0, 'Q328248', 500027985);
+INSERT INTO public.artists VALUES (108, 'Peter Alexander', 'American, 1939–2020', 'American', 'male', 1939, 2020, NULL, NULL);
+INSERT INTO public.artists VALUES (111, 'Carlo Alfano', 'Italian', 'Italian', 'male', 0, 0, NULL, NULL);
